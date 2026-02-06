44c8fbad9d75f688cd5df754e8df9ad5fe252743
Aydan Abbaszadə <aydanaab@code.edu.az>
2026-02-06 17:22:09 +0400

    Footer section completed

--- abbhome/Layouts/RootLayout/index.tsx
+++ abbhome/Layouts/RootLayout/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { ReactNode } from 'react';
 import Header from './Header';
+import Footer from './Footer';
 interface LayoutProps {
     children: ReactNode;
 }
@@ -11,6 +12,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             <main className="flex-1 w-full">
                 {children}
             </main>
+            <Footer/>
         </div>
     );
 };
